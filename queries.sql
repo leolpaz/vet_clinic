c6ae61b8a4c62a25d00e5235920a85332651a187
leolpaz <leonardolpaz95@gmail.com>
2021-11-29 16:38:40 -0300

    Create animal table schema, data and queries(last commit only added screenshot)

--- queries.sql
+++ queries.sql
@@ -1,3 +1,9 @@
-/*Queries that provide answers to the questions from all projects.*/
+SELECT * FROM animals WHERE RIGHT(name, 3) = 'mon';
+SELECT name FROM animals WHERE date_of_birth between '2016/01/01' and '2019/12/31';
+SELECT name FROM animals WHERE neutered = TRUE and escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' or name = 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = TRUE;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg between 10.4 and 17.3;
 
-SELECT * from animals WHERE name = 'Luna';
